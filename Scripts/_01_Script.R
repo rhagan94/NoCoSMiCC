ed27ce6570a7552354057292ab3097a190006ee6
rhagan94 <ryanhagan@rcsi.com>
2024-02-08 15:33:38 +0000

    Update_RScript

---
+++ Scripts/_01_Script.R
@@ -0,0 +1,14 @@
+################################################
+################################################
+
+# Author: Dr Ryan Hagan
+
+# Institute: Royal College of Surgeons in Ireland (RCSI)
+
+# Purpose: To facilitate the construction of a colonic tissue specific regulatory element map.
+
+################################################
+################################################
+
+## Load libraries
+
