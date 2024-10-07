242b35a0343eda0192fb8aa18bc55766c36dc8e5
rhagan94 <60873883+rhagan94@users.noreply.github.com>
2024-10-07 10:36:29 +0100

    Add files via upload

---
+++ Scripts/filter-long-double.py
@@ -0,0 +1,26 @@
+import sys
+from decimal import Decimal
+
+# Jill E. Moore
+# Weng Lab
+# November 2019
+
+# Script to filter out peaks over threshold accounting
+# for long doubles
+
+# TO RUN:
+# python filter-long-double.py peaks $cutoff
+
+enrichment = open(sys.argv[1])
+threshold = int(sys.argv[2].split("E-")[1])
+
+for line in enrichment:
+    fdr = str(line.rstrip().split("\t")[4])
+    if "e" not in fdr and "E" not in fdr:
+        fdr = str('%.2E' % Decimal(fdr))
+    if "+" in fdr:
+        print(line, end='')
+    else:
+        order = int(fdr.split("-")[1])
+        if order > threshold:
+            print(line, end='')
