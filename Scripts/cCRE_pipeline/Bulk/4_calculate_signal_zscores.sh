3276862746bdaddf680d98d59e62224500648dcc
rhagan94 <60873883+rhagan94@users.noreply.github.com>
2026-04-23 12:00:47 +0100

    Create 4_calculate_signal_zscores.sh

---
+++ Scripts/cCRE_pipeline/Bulk/4_calculate_signal_zscores.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+mode=$1
+files=/project/home/p201120/ryan/cCRE_pipeline/files/${mode}-List_colon.txt
+output=/project/home/p201120/ryan/cCRE_pipeline/outputs/signal-output/
+scriptDir=/project/home/p201120/ryan/cCRE_pipeline/scripts
+peaks=/project/home/p201120/ryan/cCRE_pipeline/outputs/Bulk-colon-rCARs/Bulk-rCARs-labelled.bed
+bigwigDir=/project/home/p201120/ryan/cCRE_pipeline/files/Bigwigs
+
+mkdir -p "$output"
+
+# Set widths
+if [ "$mode" == "DNase" ] || [ "$mode" == "CTCF" ] || [ "$mode" == "ATAC" ]; then
+    width=0
+else
+    width=500
+fi
+
+echo "Mode: $mode"
+
+while IFS=$'\t' read -r dset dsig _; do
+
+    echo "  Processing $dset / $dsig..."
+
+    # Temp working directory
+    tmp_dir=$(mktemp -d -t nocosmicc-XXXXXX)
+    cd "$tmp_dir" || exit 1
+
+    awk -F "\t" -v w="$width" '{printf "%s\t%.0f\t%.0f\t%s\n", $1, $2-w, $3+w, $4}' "$peaks" \
+        | awk '{if ($2 < 0) print $1 "\t" 0 "\t" $3 "\t" $4; else print $0}' \
+        | sort -u > little
+
+    # Locate bigWig
+    bw="${bigwigDir}/${mode}/${dsig}.bigWig"
+
+    # Extract signal
+    bigWigAverageOverBed -bedOut=out2.bed "$bw" little out2
+
+    # Log zscore normalisation
+    python "$scriptDir/log.zscore.normalization.py" out2 > l
+
+    # Sort and write output
+    sort -k2,2rg l \
+        | awk 'BEGIN {rank=0; before=0; running=1} {
+            if ($2 != before) rank = running
+            print $1 "\t" $2 "\t" $3 "\t" rank
+            before=$2
+            running += 1
+          }' \
+	| sort -k1,1 > "${output}/$mode/${dset}-${dsig}.txt"
+
+    echo "  Done -> ${output}/$mode/${dset}-${dsig}.txt"
+
+    cd /tmp && rm -rf "$tmp_dir"
+
+done < "$files"
