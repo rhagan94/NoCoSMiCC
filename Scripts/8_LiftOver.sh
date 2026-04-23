aa1f3478473c65e0976e5c13ffe3e85fb7174daf
rhagan94 <60873883+rhagan94@users.noreply.github.com>
2026-04-23 19:15:34 +0100

    Update 8_LiftOver.sh

--- Scripts/8_LiftOver.sh
+++ Scripts/8_LiftOver.sh
@@ -1,23 +1,30 @@
 ## This script is used to identify cCREs lifted over to the mouse and dog genomes ##
 
-# First, obtain the necessary liftOver files
-wget https://hgdownload.cse.ucsc.edu/goldenpath/mm10/vsHg38/reciprocalBest/hg38.mm10.rbest.chain.gz -P /Users/ryanhagan/NoCoSMiCC/Files
-wget https://hgdownload.soe.ucsc.edu/goldenPath/hg38/vsCanFam6/reciprocalBest/canFam6.hg38.rbest.chain.gz -P /Users/ryanhagan/NoCoSMiCC/Files
+# Define paths
+filesDir=/project/home/p201120/ryan/cCRE_pipeline/files
+liftOverDir=/project/home/p201120/ryan/cCRE_pipeline/outputs/LiftOver
+cCREs=/project/home/p201120/ryan/cCRE_pipeline/ENCODE_outputs/colon-epithelial-cCREs-Z1.28.bed
+
+# Download liftOver files
+wget https://hgdownload.cse.ucsc.edu/goldenpath/mm10/vsHg38/reciprocalBest/hg38.mm10.rbest.chain.gz -P $filesDir
+wget https://hgdownload.soe.ucsc.edu/goldenPath/hg38/vsCanFam6/reciprocalBest/hg38.canFam6.rbest.chain.gz -P $filesDir
 
 # Unzip
-gunzip /Users/ryanhagan/NoCoSMiCC/Files/*rbest.chain.gz
+gunzip $filesDir/*rbest.chain.gz
 
-# liftover human cCREs to mouse - these are mouse co-ordinates
-liftOver /Users/ryanhagan/NoCoSMiCC/ENCODE_outputs/sc-only-rCARs/MaxZ/sc-only-cCREs-2025.bed \
-/Users/ryanhagan/NoCoSMiCC/Files/hg38.mm10.rbest.chain /Users/ryanhagan/NoCoSMiCC/ENCODE_outputs/LiftOver/sc_cCRE_mouse_lifted.bed \
-/Users/ryanhagan/NoCoSMiCC/ENCODE_outputs/LiftOver/sc_cCRE_mouse_unlifted.bed -minMatch=0.5
+# liftover cCREs to mouse
+# 0.5
+liftOver $cCREs $filesDir/hg38.mm10.rbest.chain $liftOverDir/sc_cCRE_mouse_lifted.bed $liftOverDir/sc_cCRE_mouse_unlifted.bed -minMatch=0.5
+# 0.9
+liftOver $cCREs $filesDir/hg38.mm10.rbest.chain $liftOverDir/sc_cCRE_mouse_lifted_0.9.bed $liftOverDir/sc_cCRE_mouse_unlifted_0.9.bed -minMatch=0.9
 
-# liftover human cCREs to dog - these are dog co-ordinates
-liftOver /Users/ryanhagan/NoCoSMiCC/ENCODE_outputs/sc-only-rCARs/MaxZ/sc-only-cCREs-2025.bed \
-/Users/ryanhagan/NoCoSMiCC/Files/hg38.canFam6.rbest.chain  /Users/ryanhagan/NoCoSMiCC/ENCODE_outputs/LiftOver/sc_cCRE_dog_lifted.bed \
-/Users/ryanhagan/NoCoSMiCC/ENCODE_outputs/LiftOver/sc_cCRE_dog_unlifted.bed -minMatch=0.5
+# liftover cCREs to dog
+# 0.5
+liftOver $cCREs $filesDir/hg38.canFam6.rbest.chain $liftOverDir/sc_cCRE_dog_lifted.bed $liftOverDir/sc_cCRE_dog_unlifted.bed -minMatch=0.5
+# 0.9
+liftOver $cCREs $filesDir/hg38.canFam6.rbest.chain $liftOverDir/sc_cCRE_dog_lifted_0.9.bed $liftOverDir/sc_cCRE_dog_unlifted_0.9.bed -minMatch=0.9
 
-# Now filter the human cCREs and keep only those lifted over to BOTH mouse and dog
+# Filter to keep only those lifted over to BOTH mouse and dog
 # Cut
 cut -f4 /Users/ryanhagan/NoCoSMiCC/ENCODE_outputs/LiftOver/sc_cCRE_mouse_lifted.bed \
   > /Users/ryanhagan/NoCoSMiCC/ENCODE_outputs/LiftOver/cCRE_mouse_ids.txt
