a3a6c9c7d2ec65c74d476a662ed2d813dc9a0474
rhagan94 <60873883+rhagan94@users.noreply.github.com>
2026-04-27 15:02:20 +0100

    Create 2_create_rCARs.sh

---
+++ Scripts/cCRE_pipeline/Single_cell/2_create_rCARs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+dir=/project/home/p201120/ryan/cCRE_pipeline/ENCODE_outputs/colon-epithelial-rCARs
+outdir=$dir/Filtered-CARs
+
+mkdir -p "$outdir"
+cd "$dir/Processed-CARs"
+
+# Combine all samples
+echo "Combining CARs..."
+cat output.sc.* > "$dir/scATAC-CAR-All.bed"
+
+# 10th percentile cutoff
+cutoff=0.00256986
+
+# Filter
+echo "Filtering CARs..."
+awk -v cutoff="$cutoff" 'BEGIN{OFS="\t"} $6 >= cutoff' \
+    "$dir/scATAC-CAR-All.bed" | \
+sort -k1,1 -k2,2n > "$outdir/scATAC-CAR-Filtered-All.bed"
+
+echo "Picking best signal per peak..."
+sort -k4,4 -k6,6rn "$outdir/scATAC-CAR-Filtered-All.bed" | \
+    sort -k4,4 -u | \
+    sort -k1,1 -k2,2n \
+    > "$outdir/scATAC-rCAR-Filtered.bed"
+
+echo "Adding labels..."
+awk 'BEGIN{OFS="\t"} {printf "%s\t%s\t%s\tColoncCRE%d\n", $1, $2, $3, NR}' \
+    "$outdir/scATAC-rCAR-Filtered.bed" \
+    > "$outdir/scATAC-rCAR-labelled.bed"
+
+echo "Done."
+wc -l "$outdir/scATAC-rCAR-Filtered.bed"
+wc -l "$outdir/scATAC-rCAR-labelled.bed"
